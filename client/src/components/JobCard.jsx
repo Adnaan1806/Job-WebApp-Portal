67092cd556dc8d2299555ddbd7bbf3b70b06e1d9
Adnaan Janees <adnaanjanees0@gmail.com>
2024-12-23 18:48:21 +0530

    implemented the search bar and filteration functionality

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
-const JobCard = ({job}) => {
+const JobCard = ({ job }) => {
   return (
-    <div className='border p-6 transition-all duration-300 transform hover:scale-105 hover:shadow-lg rounded-lg'>
-        <div className='flex justify-between items-center'>
-            <img className='h-8' src={assets.company_icon} alt="" />
-        </div>
-        <h4 className='font-medium text-xl mt-2'>{job.title}</h4>
-        <div className='flex items-center gap-3 mt-2 text-xs'>
-            <span className='bg-blue-50 border border-blue-200 px-4 py-1.5 rounded'>{job.location}</span>
-            <span className='bg-red-50 border border-red-200 px-4 py-1.5 rounded'>{job.level}</span>
-        </div>
-        <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
-        <div className='mt-4 flex gap-4 text-sm'>
-            <button className='bg-red-600 text-white px-4 py-2 rounded transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:font-semibold'>Apply Now</button>
-            <button className='text-gray-500 border border-gray-300 rounded transition-all duration-300 hover:bg-gray-50 px-4 py-2'>Learn More</button>
-        </div>
+    <div className="border p-6 transition-all duration-300 transform hover:scale-105 hover:shadow-lg rounded-lg">
+      <div className="flex justify-between items-center">
+        <img className="h-8" src={assets.company_icon} alt="" />
+      </div>
+      <h4 className="font-medium text-xl mt-2">{job.title}</h4>
+      <div className="flex items-center gap-3 mt-2 text-xs">
+        <span className="bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">
+          {job.location}
+        </span>
+        <span className="bg-red-50 border border-red-200 px-4 py-1.5 rounded">
+          {job.level}
+        </span>
+      </div>
+      <p
+        className="text-gray-500 text-sm mt-4"
+        dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}
+      ></p>
+      <div className="mt-4 flex gap-3 text-sm">
+        <button className="bg-red-600 text-white px-4 py-2 rounded transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:font-semibold">
+          Apply Now
+        </button>
+        <button className="text-gray-500 border border-gray-300 rounded transition-all duration-300 hover:bg-gray-50 px-4 py-2">
+          Learn More
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JobCard
+export default JobCard;
