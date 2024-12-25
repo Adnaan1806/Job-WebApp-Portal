619730b75026ef609fff24648c856dfc903b880e
Adnaan Janees <adnaanjanees0@gmail.com>
2024-12-25 21:13:23 +0530

    Add Job page Ui done

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import { JobCategories, JobLocations } from "../assets/assets";
 
 const AddJob = () => {
-  return (
-    <div>AddJob</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("Bangalore");
+  const [category, setCategory] = useState("Programming");
+  const [salary, setSalary] = useState(0);
+  const [level, setLevel] = useState("Beginner level");
 
-export default AddJob
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    // initialize Quill
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
+  return (
+    <form className="conatiner p-4 flex flex-col w-full items-start gap-3">
+      <div className="w-full">
+        <p className="mb-2">Job Title</p>
+        <input
+          className="w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded"
+          type="text"
+          placeholder="Type here"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+        />
+      </div>
+
+      <div className="w-full max-w-lg">
+        <p className="my-2">Job Description</p>
+        <div className="border" ref={editorRef}></div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
+        <div>
+          <p className="mb-2">Job Category</p>
+          <select
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            {JobCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <p className="mb-2">Job Location</p>
+          <select
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+          >
+            {JobLocations.map((location, index) => (
+              <option key={index} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <p className="mb-2">Job Level</p>
+          <select
+            className="w-full px-3 py-2 border-2 border-gray-300 rounded"
+            onChange={(e) => setLevel(e.target.value)}
+            value={level}
+          >
+            <option value="Beginner Level">Beginner Level</option>
+            <option value="Intermediate Level">Intermediate Level</option>
+            <option value="Senior Level">Senior Level</option>
+          </select>
+        </div>
+      </div>
+
+      <div>
+        <p>Job Salary</p>
+        <input
+          className="w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]"
+          onChange={(e) => setSalary(e.target.value)}
+          type="Number"
+          placeholder="2500"
+          min={0}
+        />
+      </div>
+
+      <button className="w-28 mt-4 bg-red-600 text-white px-4 py-3 rounded transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:font-semibold">ADD</button>
+    </form>
+  );
+};
+
+export default AddJob;
